7e6ed047a1b85c4cf95da4c66cff8a2577c4df9a
brbr <roman.b.upqode@gmail.com>
2023-01-27 00:00:37 +0200

    first push

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Enums\Category;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/user', [UserController::class, 'index']);
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//Route::redirect('/greeting', '/home', 301);
+Route::view('/welcome/', 'set', [ 'environment' => 'aaa'] );
+//Route::get('/users/{name?}', function ($name = 'John') {
+//    return $name;
+//});
+
+Route::get('/users/{user}', function (User $user) {
+    return $user->email;
+});
+
+Route::get('/categories/{category}', function (Category $category) {
+//    var_dump( '$category', $category );
+    return $category->value;
+});
